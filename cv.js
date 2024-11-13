b9bd89e2239e2a52c01822345acf93f618c126aa
ll2Cll <149763911+ll2Cll@users.noreply.github.com>
2024-11-12 22:43:19 -0500

    Add files via upload

---
+++ cv.js
@@ -0,0 +1,51 @@
+function resumen() {
+    var info = `
+        <ul>
+            <li>
+                Soy un estudiante de ingeniería mecatrónica de últimos semestres, en busca de una oportunidad en una gran empresa.
+                Cuento con diversas habilidades ingenieriles y estoy ansioso de poder aplicar estas en proyectos reales. Mi objetivo
+                es aprender de profesionales con experiencia que me ayuden a reforzar y ampliar mis conocimientos.
+            </li>
+            <li><strong>Número de documento:</strong> 1105610013</li>
+            <li><strong>Nombres:</strong> Cristhian Johan Coque Useche</li>
+            <li><strong>Email:</strong> cristhiancoquec23@gmail.com</li>
+            <li><strong>Teléfono:</strong> +57 3227276339</li>
+            <li><strong>LinkedIn:</strong> <a href="https://linkedin.com/in/cristhian-coque-8b1731312/" target="_blank">linkedin.com/in/cristhian-coque-8b1731312</a></li>
+        </ul>
+    `;
+    $("#contenido").html(info);
+}
+
+function estudios() {
+    var info = `
+        <ul>
+            <li><strong>Ingeniería Mecatrónica:</strong> Universidad de San Buenaventura (2020 - Actual)</li>
+            <li><strong>Técnico en Sistemas:</strong> SENA - Servicio Nacional de Aprendizaje (2018-2019)</li>
+        </ul>
+    `;
+    $("#contenido").html(info);
+}
+
+function experiencia() {
+    var info = `
+        <ul>
+            <li><strong>Integrante Semillero SICON:</strong> Universidad de San Buenaventura (2023 - Actual)</li>
+            <li>Desarrollo de un proyecto de investigación enfocado en un sistema de control para un robot móvil de 5 DOF.</li>
+            <li><strong>Ponente CIIMA-2024:</strong> XII Congreso Internacional de Ingeniería Mecatrónica y Automatización</li>
+            <li>Presentación sobre cinemática de un robot de 4 eslabones y 5 DOF, tipo cuerpo-pinza.</li>
+        </ul>
+    `;
+    $("#contenido").html(info);
+}
+
+function habilidades() {
+    var info = `
+        <ul>
+            <li><strong>Lenguajes de Programación:</strong> Java, Python, C#, HTML ,CSS ,JavaScript</li>
+            <li><strong>Software:</strong> Autodesk Inventor, MATLAB, Simulink, Ansys, Excel</li>
+            <li><strong>Habilidades Blandas:</strong> Capacidad de análisis, Trabajo en equipo</li>
+            <li><strong>Idiomas:</strong> Español (Nativo), Inglés (A2)</li>
+        </ul>
+    `;
+    $("#contenido").html(info);
+}
